c27e3324ab42f425457835693d6f6e34c70492f4
Beatriz Moreli <beatrizmoreli.01@gmail.com>
2022-09-21 16:31:12 -0300

    Adcionando footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 	return (
 		<div>
 			<Navbar />
+
 			<div
 				style={{
 					height: "1000px",
 				}}
 			></div>
+
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,40 @@
+import "./footer.css";
+const Footer = () => {
+	return (
+		<div className="footer">
+			<div className="contain">
+				<div className="col">
+					<h1>Quem somos</h1>
+					<ul>
+						<li>O violeta</li>
+					</ul>
+				</div>
+				<div className="col">
+					<h1>Institucional</h1>
+					<ul>
+						<li>Entregas</li>
+						<li>Política de privacidade</li>
+						<li>Lojas</li>
+						<li>Resposabilidade social</li>
+						<li>Cashback</li>
+						<li>Fornecedor</li>
+						<li>Violeta e você</li>
+						<li>Trabalhe conosco</li>
+						<li>Tabloide de ofertas</li>
+						<li>Fale Conosco</li>
+						<li>Área do colaborador</li>
+					</ul>
+				</div>
+				<div className="col">
+					<h1>Formas de pagamento</h1>
+					<ul>
+						<li>Formas de pagamento site</li>
+						<li>Formas de pagamento loja fisica</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
